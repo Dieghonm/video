9556917724f3528af959d6b66e969828ec387d7a
Diegho <dieghonm@gmail.com>
2021-10-25 18:23:52 -0300

    client funfando

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-require 
-// const rota66 = require('./1routes/rota66');
-
 const app = express();
 const http = require('http').createServer(app);
-const port = 3000;
 
 const io = require('socket.io')(http, {
   cors: {
-  origin: 'http://localhost:3000', // url aceita pelo cors
-  methods: ['GET', 'POST'], // Métodos aceitos pela url
+    origin: 'http://localhost:3000', // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
   }});
-  
-  io.on('like', (socket) => {
+
+io.on('connection', (socket) => {
   console.log(`Usuário conectado. ID: ${socket.id} `);
-  // socket.on('like')
-  }); 
-
-// app.use(rota66);
-
-app.listen(port, () => console.log('Example app listening on port port!'));
-
-// não remova esse endpoint, e para o avaliador funcionar
-
-app.get('/', (req, res) => {
-res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
